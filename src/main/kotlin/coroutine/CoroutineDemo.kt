627c4396559c08376456ffa1a335714d220a0c1f
wwq <wangwanq@newlandcomputer.com>
2022-03-21 11:05:53 +0800

    测试了channel通道发送和接收消息

--- src/main/kotlin/coroutine/CoroutineDemo.kt
+++ src/main/kotlin/coroutine/CoroutineDemo.kt
@@ -1,12 +1,38 @@
 package coroutine
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import kotlin.concurrent.thread
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class CoroutineDemo {
+
+
+    /**
+     * 演示并测试channel的性能
+     */
+    fun TestChannel() {
+        runBlocking {
+            println("TestChannel start")
+            val channel = Channel<String>()
+            launch {
+                var count = 0
+                while (isActive) {
+                    val value = channel.receive()
+                    println("count=$count,value=$value")
+                    count++
+                    delay(2000)
+                }
+            }
+            launch {
+                for (i in 1..10) {
+                    channel.send("this is $i")
+                }
+            }
+
+        }
+    }
 
     /**
      * 测试SuspendCoroutine函数
