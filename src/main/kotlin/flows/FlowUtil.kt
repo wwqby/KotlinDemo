938825d6d2dbac377a0fadac4c53a21c89e96104
wwqby <15001243836@163.com>
2022-04-23 08:43:28 +0800

    测试了flow 验证flow执行的机制 1.flow串行会阻塞代码，执行完整个flow的emit才会继续执行 2.flow并行互不干扰，同一个flow，两次collect会完整的触发所有的emit

--- src/main/kotlin/flows/FlowUtil.kt
+++ src/main/kotlin/flows/FlowUtil.kt
@@ -1,12 +1,15 @@
 package flows
 
+import com.sun.javafx.application.LauncherImpl
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
 
 fun main(args: Array<String>) {
     runBlocking {
-        FlowUtil().testFlowCatch()
+        val util=FlowUtil()
+        util.testFlowTriggerIsBlock()
+        util.testFlowTriggerIsIndependence()
     }
 }
 
@@ -41,7 +44,7 @@ class FlowUtil {
      * 结果证明
      * 1.抛出异常后，仍然可以触发onCompletion.应该这么说，上游的异常，最后会被onCompletion收集到
      * 2.catch尽量放在onCompletion之前，不要在onCompletion中处理异常
-     * 3.collect中的异常，只能在下游处理或者通过onEach加catch组合处理
+     * 3.collect中的异常，只能在下游处理或者通过onEach加catch组合作为前置处理
      */
     suspend fun testFlowCatch()= flow{
         println("emit thread:${currentCoroutineContext()}")
@@ -101,4 +104,66 @@ class FlowUtil {
             println("collect thread:${currentCoroutineContext()}")
             println(it)
         }
+
+    /**
+     * 测试flow作为冷流的触发机制
+     * 证明flow的执行会阻塞当前位置
+     * 两个串行的flow会依次执行，执行完flow1才会执行flow2
+     */
+    suspend fun testFlowTriggerIsBlock() {
+        /**
+         * 定义一个flow对象
+         * 该flow对象会每隔一秒发送一次，共发送10次
+         */
+        val flow=flow {
+            val flowName = System.currentTimeMillis()
+            repeat(5) {
+                delay(1000)
+                emit("$flowName:$it")
+            }
+        }
+        //触发第一次
+        flow.collect{
+            println("obj1-$it")
+        }
+        //延迟1.5s以后，触发第二次
+        delay(1500)
+        flow.collect{
+            println("obj2-$it")
+        }
+    }
+
+    /**
+     * 测试flow作为冷流的触发机制
+     * 证明冷流每次被collect就会重新触发
+     * 两个并行的flow彼此互不干扰，完整触发整个flow的emit
+     */
+    suspend fun testFlowTriggerIsIndependence(){
+        /**
+         * 定义一个flow对象
+         * 该flow对象会每隔一秒发送一次，共发送10次
+         */
+        val flow=flow {
+            val flowName = System.currentTimeMillis()
+            repeat(5) {
+                delay(1000)
+                emit("$flowName:$it")
+            }
+        }
+        //发现两个flow是串行执行，第一个flow阻塞执行完毕，才执行到第二flow
+        /**
+         * 打开两个协程，分别触发两次flow
+         */
+        CoroutineScope(Dispatchers.Default).launch{
+            flow.collect{
+                println("obj3-$it")
+            }
+        }
+        delay(1500)
+        CoroutineScope(Dispatchers.Default).launch{
+            flow.collect{
+                println("obj4-$it")
+            }
+        }
+    }
 }
